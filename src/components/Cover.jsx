07f951a3485815110ad3b5b5d52e79a9fcb93202
GioSfo28 <giosfo28@gmail.com>
2024-01-05 10:44:35 +0100

    Incontri Cristiani aggiunto Like, Match e corretto errori+ cover con button funzionante

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -1,10 +1,11 @@
 import Background from '../assets/Background.jpg'
+import { Link } from "react-router-dom";
 
 function Cover (){
     return (
         <div className="h-[500px] bg-opacity-90 bg-no-repeat bg-cover  grid place-items-center" style={{ backgroundImage: `url(${Background})`}}>
             <h1 className='text-white text-6xl font-bold text-center align-middle'>Incontri Cristiani</h1>
-            <button className='bg-blue-500 hover:bg-blue-400 text-white rounded-full p-4'>Comincia subito!</button>
+            <Link to={"/Login"}><button  className='bg-blue-500 hover:bg-blue-400 text-white rounded-full p-4'>Comincia subito!</button></Link>
         </div>
     );
 }
